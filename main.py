6c2554aa8bb0062fbf874c4378732949787d67dc
Timofey <mr.tim.iv@gmail.com>
2026-02-01 16:42:24 +0300

    Пусковой файл проекта

---
+++ main.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from src.user_interface import user_interaction
+
+
+def main() -> None:
+    """
+    Точка входа в приложение.
+    """
+    user_interaction()
+
+
+if __name__ == "__main__":
+    main()
